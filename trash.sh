e09b8615b1d729f88ff493b525775a3ae502c65a
Julien Eyzat <julien.eyzat@telecomnancy.net>
2018-02-24 13:18:17 +0100

    v 4.0 (add -u and -old and fixed some bugs)

--- trash.sh
+++ trash.sh
@@ -7,21 +7,30 @@ USAGE:
     trash [option] files to remove
 
 VERSION:
-    3.0
+    4.0
 
 OPTION:
     -c clear the trash can
     -l list the files in the trash can
     -d delete a file from the trash can
-    -od delete files in the trash can that are here for more than n days
+    -old delete files in the trash can that are here for more than n days
     -v verbose mode
     -r recover a file to his last position
     -u update the recoverFile
     -debug debug
 '
 
+#Path to the directory containing the removed files
 trashPos=~/.trash/trash_can/
+#Path to the recoverFile which contains the data about files in the trash can
 recoverFile=~/.trash/recoverFile
+#This file is used to update the recoverFile
+tmp=~/.trash/tmp
+#The files which haven't a recovery directory will be send here when trying to
+#recover them
+default_recover_directory=~/
+#It is used as a delimiter in the recoverFile
+delimiter='!'
 
 #init
 mkdir -p $trashPos
@@ -46,7 +55,7 @@ if [[ $1 = "-c" ]]; then
 
 #Give the list of the files and directories in the trash can
 elif [[ $1 = "-l" ]]; then
-    list=`cut -d " " -f 1 < $recoverFile`
+    list=`cut -d $delimiter -f 1 < $recoverFile`
     printf '%s\n' $list
 
 #Show the content of the recoverFile and the trash can directory
@@ -62,22 +71,33 @@ elif [[ $1 = "-d" ]]; then
     shift 1
     for files do
         rm -r $trashPos$files
-        saveRecoverFile=`cat $recoverFile | grep -v -e "^$files"`
-        echo $saveRecoverFile > $recoverFile
+        grep -v -e "^$files" < $recoverFile > $tmp
+        cat $tmp > $recoverFile
         echo "$files deleted"
     done
 
-#NOT TESTED Delete the files older than the second argument given
-elif [[ $1 = "-od" ]]; then
+#Delete the files older than the second argument given
+elif [[ $1 = "-old" ]]; then
     if [[ $# != 2 ]]; then
-        echo "trash -r number_of_days files"
+        echo "trash -r number_of_days"
     else
-        find $trashPos -mtime +$2 -exec rm -r "{}" \;
+        # find $trashPos -mtime +$2 -exec rm -r "{}" \;
+        path_length=`find $trashPos -maxdepth 0 | tr "/" " " | wc -w`
+        real_path_length=$(($path_length+2))
+        files=`find $trashPos -mtime +$2 | cut -d '/' -f $real_path_length`
+        files_length=`echo $files | wc -w`
+        for (( i = 1; i < $(($files_length+1)); i++ )); do
+            file=`echo $files | cut -d " " -f $i`
+            rm -r $trashPos$file
+            grep -v -e "^$files" < $recoverFile > $tmp
+            cat $tmp > $recoverFile
+            echo "$files deleted"
+        done
     fi
 
 #Allow the user to recover a file or directory put in the trash can
 elif [[ $1 = "-r" ]]; then
-    if [[ $# != 2 ]]; then
+    if [[ $# < 2 ]]; then
         echo "trash -r files"
         echo "you can see the files list with trash -l"
         exit 1
@@ -85,17 +105,37 @@ elif [[ $1 = "-r" ]]; then
     shift 1
     for files do
         if [[ `grep $recoverFile -e "^$files"` ]]; then
-            last_dir=`grep -e "^$files" < $recoverFile | cut -d " " -f 2`
+            last_dir=`grep -e "^$files" < $recoverFile | cut -d $delimiter -f 2`
             mv "$trashPos$files" $last_dir
-            saveRecoverFile=`cat $recoverFile | grep -v -e "^$files"`
-            echo $saveRecoverFile > $recoverFile
+            grep -v -e "^$files" < $recoverFile > $tmp
+            cat $tmp > $recoverFile
         fi
     done
 
-#WIP Update the recoveryFile. Useful when the trash can is modified without
+#Update the recoveryFile. Useful when the trash can is modified without
 #using the trash command
+#I could also check if a file was directly removed in the trash can
 elif [[ $1 = "-u" ]]; then
-    echo "coming soon"
+    for files in $trashPos*
+    do
+        if [[ -d $files ]]; then
+            file=${files::-1}
+            directory="${files##*/}"
+            final_dir="$directory/"
+            if [[ `grep -e "^$final_dir" < $recoverFile` ]]; then
+                e=""
+            else
+                echo "$final_dir$delimiter$default_recover_directory" >> $recoverFile
+            fi
+        else
+            file="${files##*/}"
+            if [[ `grep -e "^$file" < $recoverFile` ]]; then
+                e=""
+            else
+                echo "$file$delimiter$default_recover_directory" >> $recoverFile
+            fi
+        fi
+    done
 
 #Move the files and directories in the trash can and update the recoverFile
 else
@@ -121,9 +161,9 @@ else
             final_dir=${complete_dir%/*}
             file="${complete_dir##*/}"
             if [[ -d $files ]]; then
-                echo "$file/ $final_dir" >> $recoverFile
+                echo "$file/$delimiter$final_dir" >> $recoverFile
             else
-                echo "$file $final_dir" >> $recoverFile
+                echo "$file$delimiter$final_dir" >> $recoverFile
             fi
             mv "$corrected_file" "$trashPos"
         else
